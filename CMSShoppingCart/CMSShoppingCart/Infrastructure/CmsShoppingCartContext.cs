0c5f655152b9b2e1c9882aec7d8d89ebf525fb66
Aleksei Snigirev <DMBS_el@mail.ru>
2021-06-05 14:46:21 +0300

    Initializee project, libraries, configuring dbcontext.

---
+++ CMSShoppingCart/CMSShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,15 @@
+﻿using CMSShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMSShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext :DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
---
+++ CMSShoppingCart/CMSShoppingCart/Models/Page.cs
@@ -0,0 +1,10 @@
+﻿namespace CMSShoppingCart.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Sorting { get; set; }
+    }
+}
