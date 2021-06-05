f09b15e7b6e034d27574cf12e20508105ad378ee
Aleksei Snigirev <DMBS_el@mail.ru>
2021-06-05 16:45:49 +0300

    admin areas and view

---
+++ CMSShoppingCart/CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMSShoppingCart.Infrastructure;
+using System.Threading.Tasks;
+using System.Linq;
+using CMSShoppingCart.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMSShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public PagesController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in _context.Pages
+                                     orderby p.Sorting
+                                     select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
+        }
+    }
+}
