f29187493d9275ac3920cb40be8b5fa8f73470fb
Aleksei Snigirev <DMBS_el@mail.ru>
2021-06-13 19:45:44 +0300

    fix bug with front serialization

--- CMSShoppingCart/CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -113,5 +113,23 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+        // POST /admin/pages/reorder
+        [HttpPost]
+        public async Task<IActionResult> Reorder(int[] id)
+        {
+            int count = 1;
+
+            foreach (var pageId in id)
+            {
+                Page page = await _context.Pages.FindAsync(pageId);
+                page.Sorting = count;
+                _context.Update(page);
+                await _context.SaveChangesAsync();
+                count++;
+            }
+
+            return Ok();
+        }
     }
 }
