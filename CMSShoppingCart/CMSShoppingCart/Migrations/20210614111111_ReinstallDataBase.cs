6d157f4f35309f0fe6788219e6d35c0f9fda982f
dmbsseller <DMBS_el@mail.ru>
2021-06-14 14:15:18 +0300

    re-install windows (re-install database), update ef-core to 5.0.7

---
+++ CMSShoppingCart/CMSShoppingCart/Migrations/20210614111111_ReinstallDataBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CMSShoppingCart.Migrations
+{
+    public partial class ReinstallDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Pages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Pages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
